6f0b1b720a64089a720f25b29ccab45725df74fd
chris <cb.burleigh@gmail.com>
2018-12-28 15:30:01 -0700

    changed tabs to spaces

--- benchmark.py
+++ benchmark.py
@@ -2,15 +2,15 @@ import timing
 import tictactoe
 
 def main():
-	print("running 100k games")
-	for x in range(100000):
-		playdraw()
-	
+    print("running 100k games")
+    for x in range(100000):
+        playdraw()
+    
 def playdraw():
-	board = tictactoe.TicTacToeBoard()
-	gamemoves = [(1,1), (2,1), (2,0), (0,2), (2,2), (0,0), (0,1), (1,0), (1,2)]
-	for move in gamemoves:
-			board.move(move)
+    board = tictactoe.TicTacToeBoard()
+    gamemoves = [(1,1), (2,1), (2,0), (0,2), (2,2), (0,0), (0,1), (1,0), (1,2)]
+    for move in gamemoves:
+            board.move(move)
 
 if __name__ == "__main__":
     main()
